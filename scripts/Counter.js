6e091803296b6b10df976273aa15c5fdb6e1a99e
yi-shin cheng <richard120310@gmail.com>
2022-07-11 20:54:37 +0800

    :bug: fix bug in Counter.ts

--- scripts/Counter.js
+++ scripts/Counter.js
@@ -21,7 +21,10 @@ function StopTime() {
     clearInterval(timer);
 }
 function ResetTime() {
-    var counter = document.getElementById("time_counter");
-    counter.innerHTML = "60秒";
     second = 60;
+    var time_counter = document.getElementById("time_counter");
+    var counter = document.getElementById("counter");
+    counter.style.left = "40%";
+    counter.style.top = "7%";
+    time_counter.innerHTML = second + "秒";
 }
